a03bcb56e60aa126a5ff6434528116e42c357d23
GaryCraft <garycraft@our-space.xyz>
2024-06-02 18:45:58 -0600

    Normalize code style with spaces

--- src/components/library/SearchBar.vue
+++ src/components/library/SearchBar.vue
@@ -1,23 +1,23 @@
 <template>
-	<div class="flex gap-2 items-center flex-row justify-center my-1 w-full px-2">
-		<div class="flex flex-row items-center gap-2">
-			<label for="autosearch" class="group-label">Auto</label>
-			<input type="checkbox" id="autosearch" v-model="autoSearch"
-				class="bg-brave-95 border border-brave-95 focus:border-brave-90 text-gray-900 outline-none text-sm rounded transition block min-w-fit p-1.5" />
-		</div>
-		<input type="text" id="searchInput" v-model="searchInput"
-			class="bg-brave-95 border border-brave-95 focus:border-brave-90 text-gray-900 outline-none text-sm rounded transition block w-full max-w-2xl p-1.5"
-			placeholder="Search" @keyup.enter="makeSearch" @input="handleAutoSearch" />
-		<button class="button button-normal px-4 py-1.5 rounded-full h-full" @click="makeSearch">
-			<Magnify />
-		</button>
-		<button class="button button-normal px-4 py-1.5 rounded-full h-full" @click="clearSearch">
-			<Close />
-		</button>
-		<div class="animate-spin text-sm" v-show="cooldown || preClear">
-			<Loading />
-		</div>
-	</div>
+  <div class="flex gap-2 items-center flex-row justify-center my-1 w-full px-2">
+    <div class="flex flex-row items-center gap-2">
+      <label for="autosearch" class="group-label">Auto</label>
+      <input type="checkbox" id="autosearch" v-model="autoSearch"
+        class="bg-brave-95 border border-brave-95 focus:border-brave-90 text-gray-900 outline-none text-sm rounded transition block min-w-fit p-1.5" />
+    </div>
+    <input type="text" id="searchInput" v-model="searchInput"
+      class="bg-brave-95 border border-brave-95 focus:border-brave-90 text-gray-900 outline-none text-sm rounded transition block w-full max-w-2xl p-1.5"
+      placeholder="Search" @keyup.enter="makeSearch" @input="handleAutoSearch" />
+    <button class="button button-normal px-4 py-1.5 rounded-full h-full" @click="makeSearch">
+      <Magnify />
+    </button>
+    <button class="button button-normal px-4 py-1.5 rounded-full h-full" @click="clearSearch">
+      <Close />
+    </button>
+    <div class="animate-spin text-sm" v-show="cooldown || preClear">
+      <Loading />
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -30,25 +30,25 @@ const cooldown = ref(false);
 let preClear = null;
 
 const handleAutoSearch = () => {
-	if (autoSearch.value) {
-		if (preClear) {
-			clearTimeout(preClear);
-		}
-		preClear = setTimeout(() => {
-			makeSearch();
-			preClear = null;
-		}, 500);
-	}
+  if (autoSearch.value) {
+    if (preClear) {
+      clearTimeout(preClear);
+    }
+    preClear = setTimeout(() => {
+      makeSearch();
+      preClear = null;
+    }, 500);
+  }
 };
 const makeSearch = () => {
-	cooldown.value = true;
-	useSearchLibrary().setSearch(searchInput.value);
-	setTimeout(() => {
-		cooldown.value = false;
-	}, 1000);
+  cooldown.value = true;
+  useSearchLibrary().setSearch(searchInput.value);
+  setTimeout(() => {
+    cooldown.value = false;
+  }, 1000);
 };
 const clearSearch = () => {
-	searchInput.value = '';
-	makeSearch();
+  searchInput.value = '';
+  makeSearch();
 };
 </script>
--- src/components/library/TrackList.vue
+++ src/components/library/TrackList.vue
@@ -1,32 +1,30 @@
 <template>
-	<div ref="parentRef" class="p-4 overflow-y-auto h-full" v-show="props.isActive">
-		<div :style="{ height: `${totalSize}px`, width: '100%', position: 'relative' }">
-			<div class="w-full">
-				<div class="w-full flex">
-					<div class="text-xs text-brave-30/70 font-bold flex w-full">
-						<div class="text-left flex-none w-[65%] p-1">Track</div> <!-- Adjusted width percentage -->
-						<div class="text-right flex-none w-[10%] p-1">Duration</div>
-						<div class="text-center flex-none w-[10%] p-1">Lyrics</div>
-						<div class="text-right flex-none w-[15%] p-1"></div>
-					</div>
-				</div>
-				<div class="w-full flex flex-col">
-					<div v-for="virtualRow in virtualRows" :key="virtualRow.index" class="group flex flex-col w-full"
-						:style="{
-							position: 'absolute',
-							top: 0,
-							left: 0,
-							width: '100%',
-							height: `${virtualRow.size}px`,
-							transform: `translateY(${virtualRow.start}px)`,
-						}">
-						<TrackItem :trackId="virtualRow.key" @play-track="playTrack"
-							@download-lyrics="downloadLyrics" />
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div ref="parentRef" class="p-4 overflow-y-auto h-full" v-show="props.isActive">
+    <div :style="{ height: `${totalSize}px`, width: '100%', position: 'relative' }">
+      <div class="w-full">
+        <div class="w-full flex">
+          <div class="text-xs text-brave-30/70 font-bold flex w-full">
+            <div class="text-left flex-none w-[65%] p-1">Track</div> <!-- Adjusted width percentage -->
+            <div class="text-right flex-none w-[10%] p-1">Duration</div>
+            <div class="text-center flex-none w-[10%] p-1">Lyrics</div>
+            <div class="text-right flex-none w-[15%] p-1"></div>
+          </div>
+        </div>
+        <div class="w-full flex flex-col">
+          <div v-for="virtualRow in virtualRows" :key="virtualRow.index" class="group flex flex-col w-full" :style="{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: `${virtualRow.size}px`,
+            transform: `translateY(${virtualRow.start}px)`,
+          }">
+            <TrackItem :trackId="virtualRow.key" @play-track="playTrack" @download-lyrics="downloadLyrics" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -43,22 +41,22 @@ const unfilteredTrackIds = ref([])
 const trackIds = ref([])
 
 const searchPass = async () => {
-	trackIds.value = []
-	const res = await useSearchLibrary().filter(unfilteredTrackIds.value)
-	trackIds.value = res
+  trackIds.value = []
+  const res = await useSearchLibrary().filter(unfilteredTrackIds.value)
+  trackIds.value = res
 }
 
 const parentRef = ref(null)
 
 const rowVirtualizer = useVirtualizer(
-	computed(() => ({
-		count: trackIds.value.length,
-		getScrollElement: () => parentRef.value,
-		estimateSize: () => 52,
-		overscan: 5,
-		paddingStart: 32,
-		getItemKey: (index) => trackIds.value[index]
-	}))
+  computed(() => ({
+    count: trackIds.value.length,
+    getScrollElement: () => parentRef.value,
+    estimateSize: () => 52,
+    overscan: 5,
+    paddingStart: 32,
+    getItemKey: (index) => trackIds.value[index]
+  }))
 )
 
 const virtualRows = computed(() => rowVirtualizer.value.getVirtualItems());
@@ -66,36 +64,36 @@ const virtualRows = computed(() => rowVirtualizer.value.getVirtualItems());
 const totalSize = computed(() => rowVirtualizer.value.getTotalSize())
 
 const playTrack = (track) => {
-	emit('playTrack', track)
+  emit('playTrack', track)
 }
 
 const downloadLyrics = (track) => {
-	emit('downloadLyrics', track)
+  emit('downloadLyrics', track)
 }
 
 const getTrackIds = async () => {
-	unfilteredTrackIds.value = await invoke('get_track_ids')
-	await searchPass()
+  unfilteredTrackIds.value = await invoke('get_track_ids')
+  await searchPass()
 }
 
 
 onMounted(async () => {
-	if (props.isActive) {
-		await getTrackIds()
-	}
+  if (props.isActive) {
+    await getTrackIds()
+  }
 })
 
 watch(() => props.isActive, async () => {
-	if (props.isActive) {
-		await getTrackIds()
-	}
+  if (props.isActive) {
+    await getTrackIds()
+  }
 })
 
 watch(() => useSearchLibrary().searchValue.value, async () => {
-	try {
-		await getTrackIds()
-	} catch (error) {
-		console.error(error)
-	}
+  try {
+    await getTrackIds()
+  } catch (error) {
+    console.error(error)
+  }
 })
 </script>
