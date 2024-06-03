80937c7b51e61ffa0816f6dc6c53adf697df4f0d
GaryCraft <garycraft@our-space.xyz>
2024-06-03 13:23:55 -0600

    Only necessary changes in Library.vue

--- src/components/Library.vue
+++ src/components/Library.vue
@@ -6,7 +6,6 @@
       @showConfig="isShowConfig = true"
       @showAbout="isShowAbout = true"
       @showDownloadViewer="isShowDownloadViewer = true"
-      @showSearch="toggleSearch"
     />
 
     <div v-show="isShowSearch && activeTab == 'tracks'"
@@ -15,11 +14,17 @@
     </div>
 
     <div class="relative grow overflow-hidden bg-white">
-      <TrackList :isActive="activeTab === 'tracks'" />
+      <TrackList
+        :isActive="activeTab === 'tracks'"
+      />
 
-      <AlbumList :isActive="activeTab === 'albums'" />
+      <AlbumList
+        :isActive="activeTab === 'albums'"
+      />
 
-      <ArtistList :isActive="activeTab === 'artists'" />
+      <ArtistList
+        :isActive="activeTab === 'artists'"
+      />
 
       <!-- <div class="absolute top-0 left-0 w-full h-[20px] bg-gradient-to-b from-white pointer-events-none"></div> -->
     </div>
@@ -28,13 +33,10 @@
   </div>
 
   <div v-else class="flex flex-col justify-center items-center w-full h-full">
-    <div class="animate-spin text-xl text-brave-30">
-      <Loading />
-    </div>
+    <div class="animate-spin text-xl text-brave-30"><Loading /></div>
     <div v-if="isInitializing" class="flex flex-col items-center justify-center text-sm text-brave-40">
       <div>Initializing library...</div>
-      <div v-if="initializeProgress">{{ initializeProgress.filesScanned }}/{{ initializeProgress.filesCount }}
-        files scanned</div>
+      <div v-if="initializeProgress">{{ initializeProgress.filesScanned }}/{{ initializeProgress.filesCount }} files scanned</div>
     </div>
 
     <div v-else class="flex flex-col items-center justify-center text-sm text-brave-40">
@@ -43,8 +45,7 @@
   </div>
 
   <DownloadViewer :is-show="isShowDownloadViewer" @close="closeDownloadViewer" />
-  <Config :is-show="isShowConfig" @close="isShowConfig = false" @refreshLibrary="refreshLibrary"
-    @uninitialize-library="$emit('uninitializeLibrary')" />
+  <Config :is-show="isShowConfig" @close="isShowConfig = false" @refreshLibrary="refreshLibrary" @uninitialize-library="$emit('uninitializeLibrary')" />
   <About :is-show="isShowAbout" @close="isShowAbout = false" />
 
   <Teleport to="body">
@@ -63,12 +64,12 @@ import { listen } from '@tauri-apps/api/event'
 import { Loading } from 'mdue'
 import _ from 'lodash'
 import LibraryHeader from './library/LibraryHeader.vue'
-import SearchBar from './library/SearchBar.vue'
 import NowPlaying from './NowPlaying.vue'
 import TrackList from './library/TrackList.vue'
 import AlbumList from './library/AlbumList.vue'
 import ArtistList from './library/ArtistList.vue'
 import DownloadViewer from './library/DownloadViewer.vue'
+import SearchBar from './library/SearchBar.vue'
 import Config from './library/Config.vue'
 import About from './About.vue'
 import SearchLyrics from './library/SearchLyrics.vue'
@@ -91,7 +92,6 @@ const activeTab = ref('tracks')
 const isShowDownloadViewer = ref(false)
 const isShowConfig = ref(false)
 const isShowAbout = ref(false)
-const isShowSearch = ref(false)
 
 const changeActiveTab = (tab) => {
   activeTab.value = tab
