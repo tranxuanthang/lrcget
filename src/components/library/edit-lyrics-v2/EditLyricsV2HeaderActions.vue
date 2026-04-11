dd82a5cc3bd419bf1656c3d8fbfc959f1a5d0d06
tranxuanthang <thang.dovah@gmail.com>
2026-04-11 09:36:30 +0700

    Some minor style fixes

--- src/components/library/edit-lyrics-v2/EditLyricsV2HeaderActions.vue
+++ src/components/library/edit-lyrics-v2/EditLyricsV2HeaderActions.vue
@@ -3,7 +3,7 @@
     <div class="flex-none flex">
       <VTooltip theme="lrcget-tooltip">
         <button
-          class="button text-sm px-5 py-1.5 h-8 w-24 rounded-l-full rounded-r-none border-r border-brave-95 dark:border-brave-5"
+          class="button text-sm px-5 py-1.5 h-8 w-24 rounded-l-full rounded-r-none border-r"
           :class="{ 'button-primary': isDirty, 'button-disabled': !isDirty }"
           :disabled="!isDirty"
           @click="emit('save')"
@@ -14,7 +14,10 @@
         <template #popper>
           <div class="text-xs font-bold">
             Save lyrics
-            <span class="text-[0.65rem] text-brave-30 bg-brave-95 px-1 rounded-full">Ctrl+S</span>
+            <span
+              class="text-[0.65rem] text-brave-30 bg-brave-95 px-1 rounded-full"
+              >Ctrl+S</span
+            >
           </div>
         </template>
       </VTooltip>
@@ -29,7 +32,11 @@
 
         <template #popper>
           <div class="dropdown-container">
-            <button class="dropdown-item" @click="emit('save-and-publish')" v-close-popper>
+            <button
+              class="dropdown-item"
+              @click="emit('save-and-publish')"
+              v-close-popper
+            >
               <span class="dropdown-label">Save and Publish</span>
             </button>
 
@@ -84,22 +91,22 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import ChevronDown from '~icons/mdi/chevron-down'
-import CheckboxButton from '@/components/common/CheckboxButton.vue'
+import { ref } from "vue";
+import ChevronDown from "~icons/mdi/chevron-down";
+import CheckboxButton from "@/components/common/CheckboxButton.vue";
 
-const emit = defineEmits(['save', 'save-and-publish', 'debug'])
+const emit = defineEmits(["save", "save-and-publish", "debug"]);
 
-const exportPlainText = ref(false)
-const exportSyncedLrc = ref(false)
-const exportEnhancedLrc = ref(false)
+const exportPlainText = ref(false);
+const exportSyncedLrc = ref(false);
+const exportEnhancedLrc = ref(false);
 
 defineProps({
   isDirty: {
     type: Boolean,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 </script>
 
 <style scoped>
@@ -122,5 +129,4 @@ defineProps({
 .dropdown-section-label {
   @apply text-xs uppercase font-bold text-brave-35 dark:text-brave-70 px-2 py-1;
 }
-
 </style>
